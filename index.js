cfca9083b38eadf0095d9d11250dd61c5a72297c
EvelyT <terezaevely@gmail.com>
2022-02-05 20:19:06 -0300

    começando backend.

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const listaContatos = [];
+
+const novoContato = {
+    nome: "Maria",
+    email: "maria123@gmail.com",
+    telefone: "38988654125"
+
+};
+
+listaContatos.push(novoContato);
+
+const campoNome = document.getElementById('campoNome');
+const campoEmail = document.getElementById('compoEmail');
+const campoTelefone = document.getElementById('campoTelefone');
